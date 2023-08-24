6297505472a4c18fb13ba8ece069b070852508d1
annamenacher <35665635+annamenacher@users.noreply.github.com>
2023-08-25 00:21:05 +0100

    Update firth_regression.R

--- simulation_study/scripts/firth_regression.R
+++ simulation_study/scripts/firth_regression.R
@@ -36,7 +36,7 @@ path = ""
 ########################
 
 # Data
-X = matrix(cbind(c(rep(1,N/2),rep(0,N/2)), rep(c(rep(0, (N/4)), rep(1, (N/4))),2)), nrow = N, ncol = P)
+X = matrix(cbind(c(rep(1, N/2), rep(0, N/2)), rep(c(rep(0, (N/4)), rep(1, (N/4))), 2)), nrow = N, ncol = P)
 Y = data.frame(read.csv(paste0(path_data, "Y", sim, ".csv"), header = T)[,2:(M+1)])
 
 time.start = Sys.time()
@@ -51,14 +51,14 @@ y_pred_Firth = matrix(NA, nrow = 4, ncol = M)
 # Run independent Firth regression for every voxel s_j.
 for (j in 1:M) {
   data = data.frame(cbind(Y[,j], X))
-  colnames(data) = c('y','x1','x2')
+  colnames(data) = c('y', 'x1', 'x2')
   tryCatch({
-    model = glm(y ~ 1 + x1 + x2, family = binomial(probit), data = data, method = "brglmFit", type='AS_mean', maxit = 10000, epsilon = 1e-05, slowit=0.5)
-    y_pred_Firth[,j] = fitted(model)[c(1,(N*0.25 + 1), (N*0.5 + 1),(N*0.75 + 1))]
+    model = glm(y ~ 1 + x1 + x2, family = binomial(probit), data = data, method = "brglmFit", type = 'AS_mean', maxit = 10000, epsilon = 1e-05, slowit = 0.5)
+    y_pred_Firth[,j] = fitted(model)[c(1, (N*0.25 + 1), (N*0.5 + 1),(N*0.75 + 1))]
     model = summary(model)
     params_Firth[,j] = matrix(model$coefficients[,1], nrow = (P+1), ncol = 1)
     var_Firth[,j] = matrix(model$coefficients[,2], nrow = (P+1), ncol = 1)
-  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+  }, error=function(e){cat("ERROR :", conditionMessage(e), "\n")})
 }
 time.end = Sys.time()
 (time = time.end - time.start)
